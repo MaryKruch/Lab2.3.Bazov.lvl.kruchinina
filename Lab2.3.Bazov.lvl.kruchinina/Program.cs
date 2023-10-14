ea9270985c17eb99e02dcadd53392939dcd56780
NIK <NIK@DESKTOP-MH7JFSC>
2023-10-14 21:41:46 +0200

    Add project files.

---
+++ Lab2.3.Bazov.lvl.kruchinina/Program.cs
@@ -0,0 +1,36 @@
+﻿//12 вариант
+Console.Write("Введите число от 1 до 3:");
+int n = int.Parse(Console.ReadLine());
+switch (n)
+{
+    case 1:
+        {
+
+            int a = 45, b = 13, c = -23;
+            int min = a;
+            if (b < min) min = b;
+            if (c < min) min = c;
+            Console.WriteLine(min);
+        }
+        break;
+    case 2:
+        {
+
+            int a = -31, b = 65, c = 12;
+            int min = a;
+            if (b < min) min = b;
+            if (c < min) min = c;
+            Console.WriteLine(min);
+
+        }
+        break;
+    case 3:
+        {
+            int a = 52, b = -1, c = -33;
+            int min = a;
+            if (b < min) min = b;
+            if (c < min) min = c;
+            Console.WriteLine(min);
+        }
+        break;
+}
